4073b94a9a5f2877c94ee1d04c5d84c1c633e745
Elliot Fisher <mr.efisher@gmail.com>
2015-10-01 01:08:50 -0400

    Added better error handling if no user signed in and we check for auth tokens

--- web/src/app/js/factories/httprequestintercept.factory.js
+++ web/src/app/js/factories/httprequestintercept.factory.js
@@ -3,7 +3,7 @@ angular.module("app").factory("authHttpRequestInterceptor", ['$cookieStore',
      return {
        request: function (config) {
 				 var token = $cookieStore.get('token');
-         if (token.length > 0) {
+         if (token != undefined && token.length > 0) {
            config.headers["Auth-Token"] = token;
          }
          return config;
